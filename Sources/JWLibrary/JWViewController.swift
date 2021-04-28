7ec3aedbd01cdb1fab67535e0de21e9c21626c29
jiwook <xodhks_0113@naver.com>
2021-04-28 17:56:14 +0900

    resource 변경된 내용 커밋

--- Sources/JWLibrary/JWViewController.swift
+++ Sources/JWLibrary/JWViewController.swift
@@ -6,6 +6,9 @@
 //
 /*
  ViewController 접근 확인
+ https://developer.apple.com/documentation/swift_packages/bundling_resources_with_a_swift_package
+ // 처리
+ https://useyourloaf.com/blog/add-resources-to-swift-packages/
  */
 
 import UIKit
