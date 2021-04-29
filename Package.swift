5d0f99a290bbbb4d93d376c4bd41780780630aba
jiwook <xodhks_0113@naver.com>
2021-04-29 11:51:04 +0900

    [main] 라이브러리 업데이트

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "JWLibrary",
+    platforms: [
+        .iOS(.v11) // <- 최소사용버전 (라이브러리 추가됨에 따라 필요로함)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -14,13 +17,18 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        // 해당 라이브러리는 SnapKit을 필요로 함.
+        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "JWLibrary",
-            dependencies: [],
+            dependencies: [
+                // SnapKit 사용시 dependencies 추가
+                .product(name: "SnapKit", package: "SnapKit")
+            ],
             resources:[.process("resources")]
             ),
         .testTarget(
--- Sources/JWLibrary/JWViewController.swift
+++ Sources/JWLibrary/JWViewController.swift
@@ -12,11 +12,14 @@
  */
 
 import UIKit
+import SnapKit // 드디어 import 됨.
 
 open class JWViewController: UIViewController {
     
+    var tempLabel:UILabel? = nil
+    
     open override func viewDidLoad() {
-        
+        self.initDzViews()
     }
     
     /// 생성자 추가.
@@ -24,4 +27,37 @@ open class JWViewController: UIViewController {
         let desc = JWViewController(nibName: "JWViewController", bundle: .module)
         return desc
     }
+}
+
+extension JWViewController: JWViewProtocol {
+    public func initDzViews() {
+        
+        // View 단 생성
+        if nil == tempLabel {
+            self.tempLabel = UILabel()
+            self.tempLabel?.textColor = .black
+            self.tempLabel?.text = "SnapKit 테스트 용도 라벨!!"
+            self.view.addSubview(self.tempLabel!)
+        }
+        
+        // 오토레이아웃 설정
+        self.updateDzViews()
+    }
+    
+    public func updateDzViews() {
+        tempLabel?.snp.remakeConstraints({ (make) in
+            make.leading.trailing.bottom.equalToSuperview()
+            make.height.equalTo(40)
+        })
+    }
+    
+    public func setDzNavigationViews() {
+        
+    }
+    
+    public func doDzBackAction() {
+        
+    }
+    
+    
 }
